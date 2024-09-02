1e6cbaf3d128a71f3324147ea9acc00a1b2a29cc
meluca <mell171103@gmail.com>
2024-09-02 18:46:28 -0500

    probando el proyecto de debug

--- ProyectDebug/main.cpp
+++ ProyectDebug/main.cpp
@@ -1,7 +1,7 @@
 
 /* 
  * File:   main.cpp
- * Author: BlueCod (Sammy Guergachi <sguergachi at gmail.com>)
+ * Author: BlueCod 
  *
  * Created on 2 de setiembre de 2024, 2:20 p. m.
  */
@@ -11,7 +11,7 @@
 #include <fstream>
 using namespace std;
 #define N 4
-void imprimesol(int sol[N],int n, int*paq);
+void impriMatrizSol(int sol[N],int n, int*paq);
 int empaca(int i,int n,int peso,int *paq, int sol[N]);
 /*
  * 
@@ -23,12 +23,12 @@ int main(int argc, char** argv) {
     int sol[N]{};
     
     empaca(0,n,peso,paq,sol);
-    imprimesol(sol,n,paq); 
+    impriMatrizSol(sol,n,paq); 
     
     return 0;
 }
 
-void imprimesol(int sol[N],int n, int*paq){
+void impriMatrizSol(int sol[N],int n, int*paq){
     for(int i = 0; i < n; i++) {
             if(sol[i] == 1) {
                 cout << paq[i] << " ";
@@ -36,25 +36,3 @@ void imprimesol(int sol[N],int n, int*paq){
         }
 }
 
-int empaca(int i,int n,int peso,int *paq, int sol[N]){
-    
-    if(i==n or peso<0) return 0;
-    int num = paq[i]; 
-    
-    // encuentra solucion posible
-    if(num==peso){
-        sol[i] = 1;        
-        return 1;
-    }
-
-    //peso - paq[i] -> usa el paquete
-    if(empaca(i+1,n,peso-paq[i],paq,sol)){
-        sol[i] = 1;
-        return 1; //es una posible solucion usar este paquete
-    }
-    else{ 
-        //ya no considera el paquete
-        sol[i] = 0;
-        return(empaca(i+1,n,peso,paq,sol));    
-    }
-}
