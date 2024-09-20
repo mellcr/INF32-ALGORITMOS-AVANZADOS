e783776d71a085d03c6fecbefaee4e71bd05b705
meluca <mell171103@gmail.com>
2024-09-20 16:24:59 -0500

    Ejercicio de suma de subconjuntos

--- 2-ProgDinamica/EjClasicos/2-SubConjuntoMinimo/main.cpp
+++ 2-ProgDinamica/EjClasicos/2-SubConjuntoMinimo/main.cpp
@@ -7,19 +7,24 @@
 
 #include <iostream>
 #include <vector>
-#include <algorithm>
-
-using namespace std;
-
-#include <iostream>
-#include <vector>
 #include <cmath>
-#include <algorithm>
 #include <iomanip>
-
 using namespace std;
+int versionBooleana(vector<int>& A, int n) ;
+int sumaSubConjMinimaAcumulado(vector<int>& A, int n);
 
-int particionMinimaDiferencia(vector<int>& A, int n) {
+int main() {
+    vector<int> A = {3, 7, 13, 6};
+    int n = A.size();
+    
+    cout << "La diferencia mínima es: " << sumaSubConjMinimaAcumulado(A, n) << endl;
+    //cout << "La diferencia mínima es: " << versionBooleana(A, n) << endl;
+
+    return 0;
+}
+
+
+int sumaSubConjMinimaAcumulado(vector<int>& A, int n) {
     int sumaTotal = 0;
     
     // Calculamos la suma total del conjunto
@@ -32,19 +37,53 @@ int particionMinimaDiferencia(vector<int>& A, int n) {
     // Matriz DP para almacenar si se puede lograr un subconjunto con una suma dada
     vector<vector<int>> dp(n + 1, vector<int>(capacidad + 1, 0));
     
+
+    // Llenado de la tabla DP
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= capacidad; j++) {
+            // cap > peso 
+            if (j >= A[i-1]) { //el peso entra 
+                // cap (j) >= peso
+                dp[i][j] = max(dp[i-1][j],A[i-1]+dp[i-1][j-A[i-1]]);
+            } else { 
+                // cap (j) < peso 
+                dp[i][j] = dp[i-1][j]; //simplemente el anterior
+            }
+        }
+    }
+
+    // La diferencia mínima entre los dos subconjuntos
+    int sumaMaximaSubconjunto = dp[n][capacidad]; 
+    int diferenciaMinima = sumaTotal - 2 * sumaMaximaSubconjunto;
+
+    return diferenciaMinima;
+}
+
+int versionBooleana(vector<int>& A, int n) {
+    int sumaTotal = 0;
+    
+    // Calculamos la suma total del conjunto
+    for (int i = 0; i < n; i++) {
+        sumaTotal += A[i];
+    }
+
+    // Capacidad máxima del "subconjunto" es la mitad de la suma total
+    int capacidad = sumaTotal / 2;
+    
+    // Matriz DP para almacenar si se puede lograr un subconjunto con una suma dada
+    vector<vector<bool>> dp(n + 1, vector<bool>(capacidad + 1, false));
+    
     // Inicialización: con suma 0 siempre se puede hacer con el subconjunto vacío
     for (int i = 0; i <= n; i++) {
-        dp[i][0] = 1;
+        dp[i][0] = true;
     }
 
     // Llenado de la tabla DP
     for (int i = 1; i <= n; i++) {
         for (int j = 1; j <= capacidad; j++) {
-            if (j >= A[i-1]) {
-                // cap (j) >= peso
+            if (A[i-1] <= j) {
                 dp[i][j] = dp[i-1][j] || dp[i-1][j - A[i-1]];
-            } else { 
-                // cap (j) < peso 
+            } else {
                 dp[i][j] = dp[i-1][j];
             }
         }
@@ -63,13 +102,4 @@ int particionMinimaDiferencia(vector<int>& A, int n) {
     int diferenciaMinima = sumaTotal - 2 * sumaMaximaSubconjunto;
 
     return diferenciaMinima;
-}
-
-int main() {
-    vector<int> A = {1, 6, 11, 5};
-    int n = A.size();
-    
-    cout << "La diferencia mínima es: " << particionMinimaDiferencia(A, n) << endl;
-
-    return 0;
 }
